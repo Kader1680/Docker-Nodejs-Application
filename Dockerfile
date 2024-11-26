34b88b0a588e6eb9287dfe1264ba706a57c2d57e
Kader1680 <ouldhenniabaghdad@gmail.com>
2024-11-26 11:54:27 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM node:18
+FROM node:latest
+
 WORKDIR /app
-COPY package.json .
+COPY package*.json ./
 RUN npm install
+RUN npm install -g nodemon
 COPY . .
-EXPOSE 9000
-CMD [ "npm", "start" ]
+CMD ["nodemon", "app.js"]
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express();
-const PORT = 9000;
+const PORT = 3000;
 app.listen(PORT, ()=>{
     console.log("app run sucessfuly")
 })
 
-app.get('/', (req, res)=>{res.send("hellow from the home")})
+app.get('/', (req, res)=>{res.send("hellow developer any developer dsd sds")})
+app.get('/login', (req, res)=>{res.send("hellow from the ")})
+app.get('/register', (req, res)=>{res.send("hellow from the register")})
